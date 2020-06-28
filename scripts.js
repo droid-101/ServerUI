bd5cd4ffdcb418b4a064f094f875139bb3c01d42
bricktroper <kylepinto@yahoo.ca>
2020-06-28 02:16:01 -0400

    Create sidebar template for tabs

--- scripts.js
+++ scripts.js
@@ -1,4 +1,11 @@
-function setMessage(message)
+function switchTab(name)
 {
-	document.getElementById("message").innerHTML = message
+	var tabs = document.getElementsByClassName("tab");
+
+	for (var i = 0; i < tabs.length; i++)
+	{
+		tabs[i].style.display = "none";
+	}
+
+	document.getElementById(name).style.display = "block";
 }
