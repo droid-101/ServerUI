33ee103a6a9de4215c6e9af80b2676a1cf0d7bce
droid-101 <shanepinto18@yahoo.com>
2020-06-28 12:02:44 -0400

    Switchable Tabs
    - User can now switch between tabs

--- menu.js
+++ menu.js
@@ -1,4 +1,32 @@
 function button(name)
 {
+    switch (name)
+    {
+        case "HOME":
+            restyle(name);
+            document.getElementById("home").innerHTML = "Note: This is a private server!";
+            break;
+
+        case "CONTROLS":
+            restyle(name);
+            break;
+
+        case "WORLDS":
+            restyle(name);
+            break;
+
+        case "TERMINAL":
+            restyle(name);
+            break;
+
+        case "HELP":
+            restyle(name);
+            break;
+    }
+}
+
+function restyle(name)
+{
+    document.getElementById("content").innerHTML = "";
     document.getElementById("subtitle").innerHTML = name;
 }
