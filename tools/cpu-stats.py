87b42128af97e61c3cd8f72ea82974748b98ace0
droid-101 <shanepinto18@yahoo.com>
2021-01-30 16:30:21 -0500

    Changed cpu-usage script - Truncates usage stat to 2 decimal places.

--- tools/cpu-stats.py
+++ tools/cpu-stats.py
@@ -2,7 +2,7 @@ import subprocess
 
 cpu_usage = 0
 
-out = subprocess.check_output(['mpstat', '-p', 'ALL', '1', '1'])
+out = subprocess.check_output(['mpstat', '-P', 'ALL', '1', '1'])
 out = out.decode("UTF-8")
 out = out.split("\n")
 
@@ -11,4 +11,5 @@ for i in range(4, 8):
     stats = stats.split()
     cpu_usage += float(stats[3])
 
+cpu_usage = round(cpu_usage, 2)
 print(str(cpu_usage))
