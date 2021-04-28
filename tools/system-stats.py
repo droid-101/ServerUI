c53439abf0f88f39642100f6107686ba164d022a
bricktrooper <kylepinto@yahoo.ca>
2021-04-28 14:05:44 -0400

    convert indentation to tabs in system-stats.py

--- tools/system-stats.py
+++ tools/system-stats.py
@@ -3,24 +3,24 @@
 from subprocess import getoutput
 
 def cpu_stats():
-    output = getoutput("mpstat -P ALL 1 1")
-    output = output.split("\n")
+	output = getoutput("mpstat -P ALL 1 1")
+	output = output.split("\n")
 
-    cpu_usage = 0
+	cpu_usage = 0
 
-    for i in range(4, 8):
-        stats = output[i]
-        stats = stats.split()
-        cpu_usage += float(stats[3])
+	for i in range(4, 8):
+		stats = output[i]
+		stats = stats.split()
+		cpu_usage += float(stats[3])
 
-    return "{}%".format(round(cpu_usage, 2))
+	return "{}%".format(round(cpu_usage, 2))
 
 def ram_stats():
-    output = getoutput("free -h --si | grep Mem")
-    output = output.split()
-    return "{}B".format(output[2])
+	output = getoutput("free -h --si | grep Mem")
+	output = output.split()
+	return "{}B".format(output[2])
 
 def ip_address():
-    return getoutput("dig +short myip.opendns.com @resolver1.opendns.com")
+	return getoutput("dig +short myip.opendns.com @resolver1.opendns.com")
 
 print("{} {} {}".format(cpu_stats(), ram_stats(), ip_address()))
